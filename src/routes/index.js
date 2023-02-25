69e97c57ca01d100d348b6fa472c7bcf998d5063
hasan-furkan <hsnfrkn32@gmail.com>
2023-02-26 01:00:37 +0300

    eslint configuration and prettier eslint configuration add completed

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,33 +4,33 @@ import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Login from '../pages/authentication/Login';
 import PrivateRoute from './PrivateRoute';
 import authRoutes from "./AuthRoutes";
-import LayoutRoutes from "./LayoutRoutes";
+import LayoutRoutes from './LayoutRoutes';
 
 // setup fake backend
 const Routers = () => {
-    const login = true
-    return (
-        <BrowserRouter basename={'/'}>
-            <Suspense >
-                <Routes>
-                    <Route path={'/'} element={<PrivateRoute />}>
-                        {login ? (
-                            <>
-                                <Route path={`/`} element={<Navigate to={`/dashboard`} />} />
-                            </>
-                        ) : (
-                            ''
-                        )}
-                        <Route path={`/*`} element={<LayoutRoutes />} />
-                    </Route>
-                    <Route exact path={`/login`} element={<Login />} />
-                    {authRoutes.map(({ path, Component }, i) => (
-                        <Route path={path} element={Component} key={i} />
-                    ))}
-                </Routes>
-            </Suspense>
-        </BrowserRouter>
-    );
+  const login = true;
+  return (
+    <BrowserRouter basename={"/"}>
+      <Suspense>
+        <Routes>
+          <Route path={"/"} element={<PrivateRoute />}>
+            {login ? (
+              <>
+                <Route path={"/"} element={<Navigate to={"/dashboard"} />} />
+              </>
+            ) : (
+              ""
+            )}
+            <Route path={"/*"} element={<LayoutRoutes />} />
+          </Route>
+          <Route exact path={"/login"} element={<Login />} />
+          {authRoutes.map(({ path, Component }, i) => (
+            <Route path={path} element={Component} key={i} />
+          ))}
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 };
 
 export default Routers;
