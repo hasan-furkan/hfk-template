1abb189629716590dd0b0601abf668818d7bb077
hasan-furkan <hsnfrkn32@gmail.com>
2023-02-28 10:49:57 +0300

    user logout completed

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,21 +1,40 @@
 import { Box, IconButton, useTheme } from '@mui/material';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import InputBase from '@mui/material/InputBase';
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import SearchIcon from '@mui/icons-material/Search';
 import ReactCountryFlag from 'react-country-flag';
 import { useTranslation } from 'react-i18next';
 import { ColorModeContext, tokens } from '../theme';
+import { setLogout } from '../redux/components/auth';
 
 function Navbar() {
   const { i18n } = useTranslation();
   const theme = useTheme();
   tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const dispatch = useDispatch();
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    dispatch(setLogout(false));
+    localStorage.removeItem('user');
+  };
 
   return (
     <Box display="flex" justifyContent="space-between" p={2}>
@@ -62,9 +81,14 @@ function Navbar() {
         <IconButton>
           <SettingsOutlinedIcon />
         </IconButton>
-        <IconButton>
+        <IconButton onClick={handleClick}>
           <PersonOutlinedIcon />
         </IconButton>
+        <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+          <MenuItem onClick={handleClose}>Profile</MenuItem>
+          <MenuItem onClick={handleClose}>My account</MenuItem>
+          <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        </Menu>
       </Box>
     </Box>
   );
