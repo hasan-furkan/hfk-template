cbc18c381f58c165c5cacc5a9c7584309d2c4ecf
hasan-furkan <hsnfrkn32@gmail.com>
2023-03-03 16:46:54 +0300

    navbar edited completed

--- src/navigation/horizontal/index.js
+++ src/navigation/horizontal/index.js
@@ -3,14 +3,6 @@ const naviHorizontal = [
     id: 1,
     icon: 'home',
   },
-  {
-    id: 2,
-    icon: 'sun',
-  },
-  {
-    id: 3,
-    icon: 'user',
-  },
 ];
 
 export default naviHorizontal;
