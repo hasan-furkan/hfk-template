21b24cbe878778f0f6d98444f26cfc9f956fc3c1
hasan-furkan <hsnfrkn32@gmail.com>
2023-03-03 15:31:51 +0300

    edit tailwindcss login page

--- src/pages/authentication/Login.js
+++ src/pages/authentication/Login.js
@@ -1,46 +1,31 @@
 import * as React from 'react';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import { Link, useNavigate } from 'react-router-dom';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { FormHelperText, useTheme } from '@mui/material';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { LoadingButton } from '@mui/lab';
-import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 import { validationLoginSchema } from '../../utils/auth/formValidate';
 import { errorToastMessage, succesToastMessage } from '../../components/toasts';
 import HtmlHead from '../../components/html-head/HtmlHead';
-import { tokens } from '../../theme';
-import loginBg from '../../assets/images/auth/loginBg.jpg';
 import { setUser } from '../../redux/components/auth';
-import LanguageCountry from '../../components/languageCountry';
-import FormPassword from '../../components/form/FormPassword';
 
 export default function Login() {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [loading, setLoading] = useState(false);
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
 
   const {
     register,
     formState: { errors },
-    control,
     handleSubmit,
   } = useForm({
     resolver: yupResolver(validationLoginSchema),
   });
-  const onSubmit = (data, e) => {
-    e.preventDefault();
+  const onSubmit = (data) => {
     if (data) {
-      setLoading(true);
       succesToastMessage('basarili');
       dispatch(
         setUser({
@@ -60,88 +45,98 @@ export default function Login() {
   return (
     <>
       <HtmlHead title={title} description={description} />
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <Grid
-          item
-          xs={false}
-          md={8}
-          sx={{
-            backgroundImage: `url(${loginBg})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        />
-        <Grid item xs={12} sm={12} md={4} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 24,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <LanguageCountry />
-            <Box>
-              <Typography
-                variant="h3"
-                marginBottom={2}
-                color={theme.palette.mode === 'dark' ? colors.light[300] : colors.grey[900]}
-              >
-                Welcome to HFK Theme
-              </Typography>
-              <Typography variant="body1" color={colors.grey[600]}>
-                Please sign-in to your account and start the adventure
-              </Typography>
-            </Box>
-            <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                label="Email Address"
-                autoComplete="email"
-                autoFocus
-                color="secondary"
-                {...register('email')}
-                error={!!errors.email}
-                helperText={errors?.email?.message}
+      <section className="bg-gray-50 min-h-screen flex items-center justify-center">
+        {/* login container */}
+        <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
+          {/* form */}
+          <div className="md:w-1/2 px-8 md:px-16">
+            <h2 className="font-bold text-2xl text-black-500">Login</h2>
+            <p className="text-xs mt-4 text-[#002D74] mb-8">If you are already a member, easily log in</p>
+            <form action className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
+              <input
+                className="p-2  rounded-xl border"
+                type="email"
+                name="email"
+                placeholder="Email"
+                {...register('email', { required: true })}
               />
-              <FormPassword name="password" control={control} label="Password" />
-              {!!errors.password && (
-                <FormHelperText error id="accountId-error">
-                  {errors?.password?.message}
-                </FormHelperText>
-              )}
-              <Box display="flex" justifyContent="space-between">
-                <FormControlLabel control={<Checkbox value="remember" color="secondary" />} label="Remember me" />
-                <Typography variant="body1" marginTop={1}>
-                  <Link to="/forgot-password" style={{ color: colors.purple[400] }}>
-                    Forgot password?
-                  </Link>
-                </Typography>
-              </Box>
-              <LoadingButton
-                loading={loading}
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-                color="secondary"
-              >
-                Submit
-              </LoadingButton>
-              <Box display="flex" justifyContent="center">
-                <Link to="/register" variant="body2" style={{ color: colors.purple[400] }}>
-                  {/* eslint-disable-next-line react/no-unescaped-entities */}
-                  Don't have an account? Sign Up
-                </Link>
-              </Box>
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
+              {errors.email && <span className="text-red-500 ">{errors.email?.message}</span>}
+              <div className="relative">
+                <input
+                  className="p-2 rounded-xl border w-full"
+                  type={showPassword ? 'text' : 'password'}
+                  name="password"
+                  placeholder="Password"
+                  {...register('password', { required: true })}
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={16}
+                  height={16}
+                  fill="gray"
+                  className="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer"
+                  viewBox="0 0 16 16"
+                  onClick={handleClickShowPassword}
+                >
+                  <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                  <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                </svg>
+              </div>
+              {errors.password && <span className="text-red-500 ">{errors.password?.message}</span>}
+              {/* eslint-disable-next-line react/button-has-type */}
+              <button className="bg-purple-400 rounded-xl text-light-500 py-2 hover:scale-105 duration-300">
+                Login
+              </button>
+            </form>
+            <div className="mt-6 grid grid-cols-3 items-center text-gray-400">
+              <hr className="border-gray-400" />
+              <p className="text-center text-sm">OR</p>
+              <hr className="border-gray-400" />
+            </div>
+            {/* eslint-disable-next-line react/button-has-type */}
+            <button className="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 text-[#002D74]">
+              <svg className="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px">
+                <path
+                  fill="#FFC107"
+                  d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+                />
+                <path
+                  fill="#FF3D00"
+                  d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+                />
+                <path
+                  fill="#4CAF50"
+                  d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+                />
+                <path
+                  fill="#1976D2"
+                  d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+                />
+              </svg>
+              Login with Google
+            </button>
+            <div className="mt-5 text-xs border-b border-[#002D74] py-4 text-[#002D74]">
+              <a href="#">Forgot your password?</a>
+            </div>
+            <div className="mt-3 text-xs flex justify-between items-center text-[#002D74]">
+              {/* eslint-disable-next-line react/no-unescaped-entities */}
+              <p>Don't have an account?</p>
+              {/* eslint-disable-next-line react/button-has-type */}
+              <button className="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">
+                <Link to="/">Register</Link>
+              </button>
+            </div>
+          </div>
+          {/* image */}
+          <div className="md:block hidden w-1/2">
+            <img
+              className="rounded-2xl"
+              src="https://images.unsplash.com/photo-1616606103915-dea7be788566?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
+              alt="deneme"
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 }
