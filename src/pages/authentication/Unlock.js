8cba84e6ccf3ad79df5079dffd2a3628e3db942a
hasan-furkan <hsnfrkn32@gmail.com>
2023-03-07 16:59:30 +0300

    add unlock pages, edit reset password button name

--- src/pages/authentication/Unlock.js
+++ src/pages/authentication/Unlock.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useState } from 'react';
@@ -8,11 +7,9 @@ import { errorToastMessage, succesToastMessage } from '../../components/toasts';
 import HtmlHead from '../../components/html-head/HtmlHead';
 import SvgIcons from '../../svg-icons/SvgIcons';
 import LanguageCountry from '../../components/languageCountry';
-import resetpassword from '../../assets/images/auth/resetpassword.jpg';
+import unlockaccount from '../../assets/images/auth/unlock-account.jpg';
 
-export default function ResetPassword() {
-  const navigate = useNavigate();
-
+export default function Unlock() {
   const [showPassword, setShowPassword] = useState(false);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
@@ -27,14 +24,13 @@ export default function ResetPassword() {
   const onSubmit = (data) => {
     if (data) {
       succesToastMessage('basarili');
-      navigate('/login');
     } else {
       errorToastMessage('basarisiz');
     }
   };
 
-  const title = 'Reset Password Page';
-  const description = 'Reset Password Page';
+  const title = 'Unlock Account';
+  const description = 'Unlock Account';
 
   return (
     <>
@@ -44,7 +40,7 @@ export default function ResetPassword() {
         <div className="bg-gray-100 dark:bg-black-300 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
           {/* form */}
           <div className="md:w-1/2 px-8 md:px-6">
-            <h2 className="font-bold text-2xl text-black-500">Reset Password</h2>
+            <h2 className="font-bold text-2xl text-black-500">Unlock Account</h2>
             <p className="text-xs mt-4 text-[#002D74]">If you are already a member, easily log in</p>
             <div className="mt-3 flex justify-center gap-4 mb-2">
               <LanguageCountry />
@@ -54,9 +50,9 @@ export default function ResetPassword() {
                 <input
                   className="p-2 rounded-xl border w-full"
                   type={showPassword ? 'text' : 'password'}
-                  name="oldPassword"
-                  placeholder="Old Password"
-                  {...register('oldPassword', { required: true })}
+                  name="password"
+                  placeholder="Password"
+                  {...register('password', { required: true })}
                 />
                 <div
                   role="presentation"
@@ -66,53 +62,17 @@ export default function ResetPassword() {
                   <SvgIcons width={20} height={17} icon={`${showPassword ? 'lockOn' : 'lockOff'}`} />
                 </div>
               </div>
-              {errors.oldPassword && <span className="text-red-500 ">{errors.oldPassword?.message}</span>}
-              <div className="relative">
-                <input
-                  className="p-2 rounded-xl border w-full"
-                  type={showPassword ? 'text' : 'password'}
-                  name="newPassword"
-                  placeholder="New Password"
-                  {...register('newPassword', { required: true })}
-                />
-                <div
-                  role="presentation"
-                  className="absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer"
-                  onClick={handleClickShowPassword}
-                >
-                  <SvgIcons width={20} height={17} icon={`${showPassword ? 'lockOn' : 'lockOff'}`} />
-                </div>
-              </div>
-              {errors.password && <span className="text-red-500 ">{errors.newPassword?.message}</span>}
-              <div className="relative">
-                <input
-                  className="p-2 rounded-xl border w-full"
-                  type={showPassword ? 'text' : 'password'}
-                  name="newConfirmPassword"
-                  placeholder="New Confirm Password Password"
-                  {...register('newConfirmPassword', { required: true })}
-                />
-                <div
-                  role="presentation"
-                  className="absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer"
-                  onClick={handleClickShowPassword}
-                >
-                  <SvgIcons width={20} height={17} icon={`${showPassword ? 'lockOn' : 'lockOff'}`} />
-                </div>
-              </div>
-              {errors.password && <span className="text-red-500 ">{errors.newConfirmPassword?.message}</span>}
+              {errors.oldPassword && <span className="text-red-500 ">{errors.password?.message}</span>}
+
               {/* eslint-disable-next-line react/button-has-type */}
               <button className="bg-purple-400 rounded-xl text-light-500 py-2 hover:scale-105 duration-300">
-                Submit
+                Login
               </button>
             </form>
-            <div className="mt-1 text-xs py-4 text-purple-500">
-              <Link to="/login">Go to login?</Link>
-            </div>
           </div>
           {/* image */}
           <div className="md:block hidden w-1/2">
-            <img className="rounded-2xl" src={resetpassword} alt="deneme" />
+            <img className="rounded-2xl" src={unlockaccount} alt="deneme" />
           </div>
         </div>
       </section>
--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -2,6 +2,7 @@ import Login from '../pages/authentication/Login';
 import Register from '../pages/authentication/Register';
 import ForgotPassword from '../pages/authentication/ForgotPassword';
 import ResetPassword from '../pages/authentication/ResetPassword';
+import Unlock from '../pages/authentication/Unlock';
 // import EmailVerification from '../pages/authentication/EmailVerification';
 
 const authRoutes = [
@@ -9,6 +10,7 @@ const authRoutes = [
   { path: `${process.env.PUBLIC_URL}/register`, Component: <Register /> },
   { path: `${process.env.PUBLIC_URL}/forgot-password`, Component: <ForgotPassword /> },
   { path: `${process.env.PUBLIC_URL}/reset-password`, Component: <ResetPassword /> },
+  { path: `${process.env.PUBLIC_URL}/unlock-account`, Component: <Unlock /> },
   // { path: `${process.env.PUBLIC_URL}/email-verification`, Component: <EmailVerification /> },
 ];
 
