8529dc53ae1280f9805e89e836d436145afb81a1
hasan-furkan <hsnfrkn32@gmail.com>
2023-03-07 17:24:01 +0300

    add email-verified and edit emailverification

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -3,7 +3,8 @@ import Register from '../pages/authentication/Register';
 import ForgotPassword from '../pages/authentication/ForgotPassword';
 import ResetPassword from '../pages/authentication/ResetPassword';
 import Unlock from '../pages/authentication/Unlock';
-// import EmailVerification from '../pages/authentication/EmailVerification';
+import EmailVerification from '../pages/authentication/EmailVerification';
+import EmailVerified from '../pages/authentication/EmailVerified';
 
 const authRoutes = [
   { path: `${process.env.PUBLIC_URL}/login`, Component: <Login /> },
@@ -11,7 +12,8 @@ const authRoutes = [
   { path: `${process.env.PUBLIC_URL}/forgot-password`, Component: <ForgotPassword /> },
   { path: `${process.env.PUBLIC_URL}/reset-password`, Component: <ResetPassword /> },
   { path: `${process.env.PUBLIC_URL}/unlock-account`, Component: <Unlock /> },
-  // { path: `${process.env.PUBLIC_URL}/email-verification`, Component: <EmailVerification /> },
+  { path: `${process.env.PUBLIC_URL}/email-verification`, Component: <EmailVerification /> },
+  { path: `${process.env.PUBLIC_URL}/email-verified`, Component: <EmailVerified /> },
 ];
 
 export default authRoutes;
